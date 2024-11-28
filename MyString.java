d48c0cb4e080f9a03a5aa82910551482b14ed080
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2024-11-28 14:46:59 +0200

    MyString

--- MyString.java
+++ MyString.java
@@ -4,7 +4,7 @@ public class MyString {
         System.out.println("UnHappy : " + lowerCase("UnHappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
+        System.out.println("lowercase : " + lowerCase("lowercase"));  
 
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
@@ -13,18 +13,39 @@ public class MyString {
         System.out.println(contains("psychology", "psycho")); // true
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        System.out.println(contains("resignation", "sign")); // true 
     }
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String low = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 65 && str.charAt(i) <= 90)
+                low = low + (char)(str.charAt(i) + 32);
+            else
+                low = low + str.charAt(i);
+        }
+        return low;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        String low1 = lowerCase(str1);
+        String low2 = lowerCase(str2);
+        boolean isTrue = false;
+        for (int i = 0; i < low1.length(); i++) {
+            if (low1.charAt(i) == low2.charAt(0)) {
+                isTrue = true;
+                for (int k = 0; k < low2.length(); k++) {
+                    if (low1.charAt(k + i) != low2.charAt(k)) {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if (isTrue)
+                    return isTrue;
+            }
+        }
+        return isTrue;
     }
 }
