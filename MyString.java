44f96a36aac98547468008f0cf801cad62ffa6c5
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2024-11-30 23:44:06 +0200

    pleasepleaseplease

--- MyString.java
+++ MyString.java
@@ -30,6 +30,8 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
+        if (str2.length() == 0)
+            return true;
         if (str1.length() < str2.length())
             return false;
         boolean isTrue = false;
